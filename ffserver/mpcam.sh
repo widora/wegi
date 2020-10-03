f7005929950e3c56d0be66258af308193fbf6da9
midas-zhou <midaszhou@yahoo.com>
2020-10-03 13:11:11 +0800

    Test: ffserver CAM

---
+++ ffserver/mpcam.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+## To play rstp CAM stream.
+## Example: ./mpcam.sh rtsp://192.168.8.1:5456/test.avi
+
+while [ 1 ]
+do
+  ## -vf scale=240:176 -fs  -vf pp=hb/vb/dr/al  -rtsp-stream-over-tcp
+  ## -vfm ffmpeg: Can greatly improve AV sync! but NOT here
+  ## For'Too many buffered pts':  -nocorrect-pts
+  ## NOSYNC: -autosync 0 -mc 0, SYNC: -autosync 30 -mc 2.0
+  ## -brightness 12
+  mplayer -nocorrect-pts ${*} -framedrop -fs -vf scale=240:176
+  #mplayer -nocorrect-pts ${*} -framedrop -fs -vf rotate=2
+  sleep 1
+done
